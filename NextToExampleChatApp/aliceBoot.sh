777c553cededf3856b476b6500de308f07a7cb58
ScottWilliamAnderson <47524721+ScottWilliamAnderson@users.noreply.github.com>
2022-03-25 13:12:59 +0000

    updated server IP

--- NextToExampleChatApp/aliceBoot.sh
+++ NextToExampleChatApp/aliceBoot.sh
@@ -1,1 +1,1 @@
-dotnet exec ExampleChatApplication-1.0.5/ChatClient/bin/Release/netcoreapp5.0/ChatClient.dll Alice 10.0.0.3
+dotnet exec ExampleChatApplication-1.0.5/ChatClient/bin/Release/netcoreapp5.0/ChatClient.dll Alice 10.0.0.6
