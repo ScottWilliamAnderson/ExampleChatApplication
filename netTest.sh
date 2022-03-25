d3fdd16f938e6605dda792f683e8b5cd670cce3b
ScottWilliamAnderson <47524721+ScottWilliamAnderson@users.noreply.github.com>
2022-03-25 13:11:59 +0000

    changed server IP to what it should be

--- netTest.sh
+++ netTest.sh
@@ -11,31 +11,31 @@ xdotool key --window "$(xdotool search --name 'Node: server')" Return
 
 sleep 2
 
-xdotool type --window "$(xdotool search --name 'Node: h1')" 'iperf -i 10 -t 60 -c 10.0.0.3' 
+xdotool type --window "$(xdotool search --name 'Node: h1')" 'iperf -i 10 -t 60 -c 10.0.0.6' 
 sleep 1
 xdotool key --window "$(xdotool search --name 'Node: h1')" Return
 
 sleep 65
 
-xdotool type --window "$(xdotool search --name 'Node: h2')" 'iperf -i 10 -t 60 -c 10.0.0.3' 
+xdotool type --window "$(xdotool search --name 'Node: h2')" 'iperf -i 10 -t 60 -c 10.0.0.6' 
 sleep 1
 xdotool key --window "$(xdotool search --name 'Node: h2')" Return
 
 sleep 65
 
-xdotool type --window "$(xdotool search --name 'Node: h3')" 'iperf -i 10 -t 60 -c 10.0.0.3' 
+xdotool type --window "$(xdotool search --name 'Node: h3')" 'iperf -i 10 -t 60 -c 10.0.0.6' 
 sleep 1
 xdotool key --window "$(xdotool search --name 'Node: h3')" Return
 
 sleep 65
 
-xdotool type --window "$(xdotool search --name 'Node: h4')" 'iperf -i 10 -t 60 -c 10.0.0.3' 
+xdotool type --window "$(xdotool search --name 'Node: h4')" 'iperf -i 10 -t 60 -c 10.0.0.6' 
 sleep 1
 xdotool key --window "$(xdotool search --name 'Node: h4')" Return
 
 sleep 65
 
-xdotool type --window "$(xdotool search --name 'Node: h5')" 'iperf -i 10 -t 60 -c 10.0.0.3' 
+xdotool type --window "$(xdotool search --name 'Node: h5')" 'iperf -i 10 -t 60 -c 10.0.0.6' 
 sleep 1
 xdotool key --window "$(xdotool search --name 'Node: h5')" Return
 
